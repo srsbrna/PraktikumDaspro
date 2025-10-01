25433fe2d68ac58b79b233cbc3e9256a52e5e5d8
srsbrna <srsbkusumadewi@gmail.com>
2025-10-01 15:50:12 +0700

    Tugas 1

--- jobsheet5/nestedUjianSkripsi.java
+++ jobsheet5/nestedUjianSkripsi.java
@@ -9,14 +9,14 @@ public class nestedUjianSkripsi {
         System.out.println("Apakah mahasiswa sudah bebas kompen? (ya/tidak): ");
         String bebasKompen = sc.nextLine().trim();//trim digunakan untuk menghapus spasi sebelum dan setelah string
 
+        if (bebasKompen.equalsIgnoreCase("ya")) {
         System.out.println("Masukkan jumlah log bimbingan pembimbing 1: ");
         int bimbinganP1 = sc.nextInt();
 
         System.out.println("Masukkan jumlah log bimbingan pembimbing 2: ");
         int bimbinganP2 = sc.nextInt();
 
-        if (bebasKompen.equalsIgnoreCase("ya")) {
-            if (bimbinganP1 >= 8 && bimbinganP2 >= 4) {
+        if (bimbinganP1 >= 8 && bimbinganP2 >= 4) {
                 pesan = "Semua syarat terpenuhi. Mahasiswa boleh mendaftar ujian skripsi";
             } else if (bimbinganP1 < 8 && bimbinganP2 < 4) {
                 pesan = "Gagal! Log bimbingan P1 kurang dari 8 kali dan P2 kurang dari 4 kali";
