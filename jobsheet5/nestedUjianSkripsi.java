3d517c71ab4e8d2a2da2558a0a67ddbc342d0787
srsbrna <srsbkusumadewi@gmail.com>
2025-10-01 13:08:11 +0700

    percobaan 3

---
+++ jobsheet5/nestedUjianSkripsi.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+
+public class nestedUjianSkripsi {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String pesan;
+
+        System.out.println("Apakah mahasiswa sudah bebas kompen? (ya/tidak): ");
+        String bebasKompen = sc.nextLine().trim();//trim digunakan untuk menghapus spasi sebelum dan setelah string
+
+        System.out.println("Masukkan jumlah log bimbingan pembimbing 1: ");
+        int bimbinganP1 = sc.nextInt();
+
+        System.out.println("Masukkan jumlah log bimbingan pembimbing 2: ");
+        int bimbinganP2 = sc.nextInt();
+
+        if (bebasKompen.equalsIgnoreCase("ya")) {
+            if (bimbinganP1 >= 8 && bimbinganP2 >= 4) {
+                pesan = "Semua syarat terpenuhi. Mahasiswa boleh mendaftar ujian skripsi";
+            } else if (bimbinganP1 < 8 && bimbinganP2 < 4) {
+                pesan = "Gagal! Log bimbingan P1 kurang dari 8 kali dan P2 kurang dari 4 kali";
+            } else if (bimbinganP1 < 8){
+                pesan = "Gagal! Log bimbingan P1 belum mencapai 8 kali";
+            } else {
+                pesan = "Gagal! Log bimbingan P2 belum mencapai 4 kali" ;
+            }
+        } else {
+            pesan = "Gagal! Mahasiswa masih memiliki tanggungan kompen";
+        }
+        System.out.println(pesan);
+        sc.close();
+    }
+}
