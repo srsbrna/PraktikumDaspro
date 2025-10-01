4e22796caab9cb77a07368b760bfb4287fd8bfc5
srsbrna <srsbkusumadewi@gmail.com>
2025-10-01 15:46:14 +0700

    Tugas 1

--- jobsheet5/ifCetakKRS25.java
+++ jobsheet5/ifCetakKRS25.java
@@ -2,16 +2,20 @@ import java.util.Scanner;
 public class ifCetakKRS25 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String pesan;
 
         System.out.println("--- Cetak KRS SIAKAD ---");
         System.out.println("Apakah UKT sudah lunas? (true/false): ");
         boolean uktLunas = sc.nextBoolean();
 
-        if (uktLunas) {
-            System.out.println("Pembayaran UKT terverifikasi");
-            System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
-        }  else {
-            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
-        }
+        pesan = (uktLunas) ? "Pembayaran terverifikasi, silahkan cetak KRS dan minta tanda tangan DPA" : "Registrasi ditolak. Silakan lunasi UKT terlebih dahulu";
+
+        System.out.println(pesan);
+        //if (uktLunas) {
+        //    System.out.println("Pembayaran UKT terverifikasi");
+        //    System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+        //}  else {
+        //    System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
+        //}
     }
 }
