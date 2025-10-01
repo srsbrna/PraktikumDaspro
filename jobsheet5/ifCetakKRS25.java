b8e24dc0afc694077705b3da981aaa06251df532
srsbrna <srsbkusumadewi@gmail.com>
2025-10-01 10:13:56 +0700

    modifikasi percobaan 1

--- jobsheet5/ifCetakKRS25.java
+++ jobsheet5/ifCetakKRS25.java
@@ -10,6 +10,8 @@ public class ifCetakKRS25 {
         if (uktLunas) {
             System.out.println("Pembayaran UKT terverifikasi");
             System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+        }  else {
+            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
         }
     }
 }
