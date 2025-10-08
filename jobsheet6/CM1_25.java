c329c1a3e11517ca4a088dfdf3b9aeb2eeda62ec
srsbrna <srsbkusumadewi@gmail.com>
2025-10-08 11:17:59 +0700

    TUGAS

---
+++ jobsheet6/CM1_25.java
@@ -0,0 +1,20 @@
+package PraktikumDaspro.jobsheet6;
+
+import java.util.Scanner;
+
+public class CM1_25 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in)
+
+            System.out.println("/n==== INPUT DATA MAHASISWA ====");
+
+            System.out.println("Masukkan Nama = ");
+            nama=input.nextLine();
+
+            System.out.println("Masukkan NIM = ");
+            nim=input.nextLine();
+
+            System.out.println("/n--- Mata Kuliah 1 ---");
+
+    }
+}
