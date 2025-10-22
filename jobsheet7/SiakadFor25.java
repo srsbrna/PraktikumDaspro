08c7c51d1678ea1c80df558ecde98b296aa706da
srsbrna <srsbkusumadewi@gmail.com>
2025-10-22 09:21:50 +0700

    changes2

--- jobsheet7/SiakadFor25.java
+++ jobsheet7/SiakadFor25.java
@@ -7,6 +7,8 @@ public class SiakadFor25 {
         Scanner sc = new Scanner(System.in);
 
         double nilai, tertinggi = 0, terendah = 100;
+        int lulus = 0, tidakLulus = 0;
+
         for (int i = 1; i <= 10; i++) {
             System.out.println("Masukkan nilai mahasiswa ke-" + i + ": ");
             nilai = sc.nextDouble();
@@ -16,9 +18,15 @@ public class SiakadFor25 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+            if (nilai >= 60) {
+                lulus++;
+            } else {
+                tidakLulus++;
+            }
         }
-
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Jumlah mahasiswa lulus: " + lulus);
+        System.out.println("Jumlah mahasiswa tidak lulus: " + tidakLulus);
     }
 }
