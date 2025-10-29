732b0c3d6050dfc092b8d84be2ae2df869355e4e
srsbrna <srsbkusumadewi@gmail.com>
2025-10-29 10:55:40 +0700

    changes 1

---
+++ jobsheet8/Kotakangka25.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Kotakangka25 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai n (minimal 3) : ");
+        int n = sc.nextInt();
+
+        if (n < 3) {
+            System.out.println("Nilai n minimal 3!");
+            return;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || i == n || j == 1 || j == n ) {
+                    System.out.print(n + " ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        sc.close();
+        }
+    }
+}
