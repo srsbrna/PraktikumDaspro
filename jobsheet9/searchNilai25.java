29cf7e9507a656c54fc4265a703266b66893f64f
srsbrna <srsbkusumadewi@gmail.com>
2025-11-05 11:39:44 +0700

    changes 3

--- jobsheet9/searchNilai25.java
+++ jobsheet9/searchNilai25.java
@@ -24,5 +24,10 @@ public class searchNilai25 {
                 break;
             }
         }
+
+        if(!found){
+            System.out.println();
+            System.out.println("Nilai yang dicari tidak ditemukan");
+        }
     }
 }
