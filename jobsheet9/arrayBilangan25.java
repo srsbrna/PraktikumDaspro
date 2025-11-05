d18e612a5cfc0cad5c40cf8fcd0e1ddca0fc585c
srsbrna <srsbkusumadewi@gmail.com>
2025-11-05 09:07:15 +0700

    changes2

--- jobsheet9/arrayBilangan25.java
+++ jobsheet9/arrayBilangan25.java
@@ -9,10 +9,12 @@ public class arrayBilangan25 {
         bil[2] = -7;
         bil[3] = 17;
 
-        System.out.println(bil[0]);
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
+        for (int i = 0; i < 4; i++)
+            System.out.println(bil[i]);
+        //System.out.println(bil[0]);
+        //System.out.println(bil[1]);
+        //System.out.println(bil[2]);
+        //System.out.println(bil[3]);
         
     }
 }
