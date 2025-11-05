ae4cda350c988788b5fca6b35a596ae8f0695a83
srsbrna <srsbkusumadewi@gmail.com>
2025-11-05 09:13:46 +0700

    changes 3

--- jobsheet9/arrayBilangan25.java
+++ jobsheet9/arrayBilangan25.java
@@ -2,12 +2,12 @@ package jobsheet9;
 
 public class arrayBilangan25 {
     public static void main(String[] args) {
-        int[] bil = new int[4];
+        int[] bil = {5, 13, -7, 17};
         
-        bil[0] = 5;
-        bil[1] = 13;
-        bil[2] = -7;
-        bil[3] = 17;
+        //bil[0] = 5;
+        //bil[1] = 13;
+        //bil[2] = -7;
+        //bil[3] = 17;
 
         for (int i = 0; i < 4; i++)
             System.out.println(bil[i]);
