a4fb38819ae02da49c631d6fb69a5c2649b97705
srsbrna <srsbkusumadewi@gmail.com>
2025-11-12 11:24:16 +0700

    changes 2

--- jobsheet10/BioskopWithScanner25.java
+++ jobsheet10/BioskopWithScanner25.java
@@ -6,29 +6,38 @@ public class BioskopWithScanner25 {
         Scanner sc = new Scanner(System.in);
         
         String[][] penonton = new String[4][2];
-        String nama, next;
-        int baris, kolom;
+        int menu;
 
-        while (true) {
-            System.out.println("Masukkan nama: ");
-            nama = sc.nextLine();
-            System.out.println("Masukkan baris: ");
-            baris = sc.nextInt();
-            System.out.println("Masukkan kolom: ");
-            kolom = sc.nextInt();
+        do { 
+            System.out.println("\n=== MENU BIOSKOP ===");
+            System.out.println("1. Input Data Penonton");
+            System.out.println("2. Tampilkan Daftar Penonton");
+            System.out.println("3. Exit");
+            System.out.print("Pilih menu: ");
+            menu = sc.nextInt();
             sc.nextLine();
 
-            penonton[baris-1][kolom-1] = nama;
+            if (menu == 1){
+                System.out.print("Masukkan nama penonton: ");
+                String nama = sc.nextLine();
+                System.out.print("Masukkan nomor baris (0-3): ");
+                int baris = sc.nextInt();
+                System.out.println("Masukkan nomor kolom (0-1): ");
+                int kolom = sc.nextInt();
+                sc.nextLine();
 
-            System.out.print("Input penonton lainnya? (y/n): ");
-            next = sc.nextLine();
-
-            if (next.equalsIgnoreCase("n")){
-                break;
+                
+            } else if (menu == 2){
+                System.out.println("\nDaftar Penonton: ");
+                for (int i = 0; i < penonton.length; i++) {
+                    for (int j = 0; j < penonton[i].length; j++) {
+                        //System.out.print((penonton[i][j] == null ? ));
+                    }
+                }
             }
-        }
+        } while (menu != 3);
 
-
+        
         sc.close();
     }
 }
