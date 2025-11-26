82144d8a7a9556d10d969fa9aa6a984c3a323906
srsbrna <srsbkusumadewi@gmail.com>
2025-11-26 08:22:42 +0700

    changes 1

---
+++ Jobsheet12/kafe25.java
@@ -0,0 +1,19 @@
+package Jobsheet12;
+
+public class kafe25 {
+    public static void menu() {
+        System.out.println("===== MENU RESTO KAFE =====");
+        System.out.println("1. Kopi Hitam - Rp15.000");
+        System.out.println("2. Cappuccino - Rp20.000");
+        System.out.println("3. Latte - Rp22.000");
+        System.out.println("5. Roti Bakar - Rp10.000");
+        System.out.println("6. Mie Goreng - Rp18.000");
+        System.out.println("===========================");
+        System.out.println("Silahkan pilih menu yang Anda inginkan");
+    }
+
+    public static void main(String[] args) {
+        menu();
+    }
+    
+}
