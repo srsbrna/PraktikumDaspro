0d8e65fe5b25a41ac99a426350887de0f49a7d0f
srsbrna <srsbkusumadewi@gmail.com>
2025-12-18 12:55:30 +0700

    changes 2

--- casemethod16/CM2Magang25.java
+++ casemethod16/CM2Magang25.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class CM2Magang25 {
     static Scanner sc = new Scanner(System.in);
 
-    static String[][] dataMagang = new String[100][6]; //array untuk menyimpan data
+    static String[][] dataMagang = new String[100][7]; //array untuk menyimpan data
     static int jumlahData = 0; //menyimpan jumlah data yang sudah di masukkan
 
     public static void main(String[] args) {
@@ -12,7 +12,7 @@ public class CM2Magang25 {
         do {
             tampilMenu(); //menampilkan menu
 
-            System.out.println("Pilih menu (1-5): ");
+            System.out.println("Pilih menu (1-6): ");
             pilih = sc.nextInt();
             sc.nextLine();
 
@@ -21,21 +21,24 @@ public class CM2Magang25 {
                     tambahData();
                     break;
                 case 2:
-                    tampilData();
+                    tampilSemuaData();
                     break;
                 case 3:
-                    cariBerdasarProdi();
+                    tampilBerdasarProdi();
                     break;
                 case 4:
                     hitungJumlahStatus();
                     break;
                 case 5:
+                    tampilPerusahaan();
+                    break;
+                case 6:
                     System.out.println("Keluar...");;
                     break;
                 default:
                     System.out.println("Pilihan tidak valid!");
             }
-        } while (pilih != 5); //perulangan berhenti jika pilih 5
+        } while (pilih != 6); //perulangan berhenti jika pilih 5
     }
 
     static void tampilMenu() {//fungsi untuk menampilkan menu
@@ -44,7 +47,8 @@ public class CM2Magang25 {
         System.out.println("2. Tampilkan Semua Pendaftar Magang");
         System.out.println("3. Cari Pendaftar berdasarkan Program Studi");
         System.out.println("4. Hitung Jumlah Pendaftar untuk Setiap Status");
-        System.out.println("5. Keluar");
+        System.out.println("5. Cari pendaftar berdasarkan Perusahaan");
+        System.out.println("6. Keluar");
     }
 
      static void tambahData() { //fungsi untuk menambahkan data baru
@@ -54,7 +58,14 @@ public class CM2Magang25 {
         dataMagang[jumlahData][0] = sc.nextLine();
 
         System.out.print("NIM: ");
-        dataMagang[jumlahData][1] = sc.nextLine();
+        String nim = sc.nextLine();
+        
+        if (nim.trim().isEmpty()) { //menambahkan syarat untuk membuat program berhenti ketika user tidak menginputkan nim
+            System.out.println("NIM tidak boleh kosong! Data tidak disimpan.");
+            return; // keluar dari method tambahData
+        }
+
+        dataMagang[jumlahData][1] = nim;
 
         System.out.print("Program Studi: ");
         dataMagang[jumlahData][2] = sc.nextLine();
@@ -84,10 +95,8 @@ public class CM2Magang25 {
     }
 
 
-    static void tampilData() { //fungsi untuk menampilkan data berdasarkan prodi
-        System.out.println("\n== Sistem Pendaftaran Magang Mahasiswa ==");
-        System.out.print("Masukkan Program Studi: ");
-        String cari = sc.nextLine(); //meminta user untuk memasukkan prodi yang akan di cari
+    static void tampilSemuaData() { //fungsi untuk menampilkan data berdasarkan prodi
+        System.out.println("\n== Data Pendaftar Magang Mahasiswa ==");
 
         System.out.println("---------------------------------------------------------------------------------------------------------------");
         System.out.printf("| %-3s | %-15s | %-10s | %-15s | %-20s | %-8s | %-10s |\n",
@@ -96,8 +105,34 @@ public class CM2Magang25 {
 
         int no = 1;
         for (int i  = 0; i < jumlahData; i++) { //perulangan untuk semua data
-            if (dataMagang[i][2].equalsIgnoreCase(cari)) { //filter berdasarkan prodi
             System.out.printf("%-3d %-15s %-12s %-20s %-25s %-9s %-10s\n",
+                    no++,
+                    dataMagang[i][0],
+                    dataMagang[i][1],
+                    dataMagang[i][2],
+                    dataMagang[i][3],
+                    dataMagang[i][4],
+                    dataMagang[i][5]);
+        }
+    }
+
+    static void tampilBerdasarProdi() { //fungsi untuk menjalankan pencarian
+        System.out.println("\n== Data Pendaftar Berdasarkan Program Studi ==");
+        System.out.println("Masukkan Program Studi: ");
+        String cari = sc.nextLine();
+
+        System.out.println("---------------------------------------------------------------------------------------------------------------");
+        System.out.printf("| %-3s | %-15s | %-10s | %-15s | %-20s | %-8s | %-10s |\n",
+        "No", "Nama", "NIM", "Prodi", "Perusahaan", "Semester", "Status");
+        System.out.println("---------------------------------------------------------------------------------------------------------------");
+
+        int no = 1;
+        boolean ada = false;
+
+        for (int i = 0; i < jumlahData; i++) {
+            if (dataMagang[i][2].equalsIgnoreCase(cari)) {
+                ada = true;
+                System.out.printf("%-3d %-15s %-12s %-20s %-25s %-9s %-10s\n",
                     no++,
                     dataMagang[i][0],
                     dataMagang[i][1],
@@ -107,10 +142,6 @@ public class CM2Magang25 {
                     dataMagang[i][5]);
             }
         }
-    }
-
-    static void cariBerdasarProdi() { //fungsi untuk menjalankan pencarian
-        tampilData(); //memanggil fungsi
     }
 
     static void hitungJumlahStatus() { //fungsi untuk menghitung jumlah status
@@ -129,6 +160,36 @@ public class CM2Magang25 {
         System.out.println("\nDiterima : " + diterima);
         System.out.println("Menunggu : " + menunggu);
         System.out.println("Ditolak  : " + ditolak);
-        System.out.println("Total    : " + jumlahData);
+        System.out.println("Total    : " + jumlahData); 
+    }
+    static void tampilPerusahaan() { //fungsi untuk menjalankan pencarian
+        System.out.println("\n== Data Pendaftar Berdasarkan Perusahaan yang dituju ==");
+        System.out.println("Masukkan Nama Perusahaan: ");
+        String cariP = sc.nextLine();
+
+        System.out.println("---------------------------------------------------------------------------------------------------------------");
+        System.out.printf("| %-3s | %-15s | %-10s | %-15s | %-20s | %-8s | %-10s |\n",
+        "No", "Nama", "NIM", "Prodi", "Perusahaan", "Semester", "Status");
+        System.out.println("---------------------------------------------------------------------------------------------------------------");
+
+        int no = 1;
+        boolean ada = false;
+
+        for (int i = 0; i < jumlahData; i++) {
+            if (dataMagang[i][3].equalsIgnoreCase(cariP)) {
+                ada = true;
+                System.out.printf("%-3d %-15s %-12s %-20s %-25s %-9s %-10s\n",
+                    no++,
+                    dataMagang[i][0],
+                    dataMagang[i][1],
+                    dataMagang[i][2],
+                    dataMagang[i][3],
+                    dataMagang[i][4],
+                    dataMagang[i][5]);
+            }
+        }
+        if (!ada) {
+        System.out.println("Data pendaftar dengan perusahaan tersebut tidak ditemukan.");
+        }
     }
 }
